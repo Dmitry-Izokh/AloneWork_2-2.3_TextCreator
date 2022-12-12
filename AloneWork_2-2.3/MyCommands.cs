ba3b4d3e4b4ac5d1300d9f1591ef6d9836c00a20
Дмитрий Изох <izokh_dv@mail.ru>
2022-12-12 23:08:16 +0300

    Внес дополнения в программу. Запутался. Компилятор указывает на ошибки. Свойства прекратили отображаться, не могу посмотреть не свойства графических элементов ни их события

---
+++ AloneWork_2-2.3/MyCommands.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AloneWork_2_2._3
+{
+    class MyCommands
+    {
+        public static RoutedCommand Exit{get; set;}
+           
+            static MyCommands()
+            {
+            InputGestureCollection input = new InputGestureCollection();
+            input.Add(new KeyGesture(Key.Q, ModifierKeys.Control, "Ctrl+Q"));
+            Exit = new RoutedCommand("Выход", typeof(MyCommands), input);
+            }
+
+        public static RoutedCommand SaveAs { get; set; }
+            static MyCommands()
+        {
+            InputGestureCollection input_1 = new InputGestureCollection();
+            input_1.Add(new KeyGesture(Key.S, ModifierKeys.Control, "Ctrl+S"));
+            SaveAs = new RoutedCommand("Сохранить как", typeof(MyCommands), input_1);
+        }
+    }
+}
